f00d2fee22f7b08be7687f1c1533d28aafe9de7b
Quinn Cheong <quinncheong@hotmail.com>
2021-08-06 11:17:51 +0800

    Implemented add task component and binding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button text="Add Task" color="Green" />
+        <Button 
+            @btn-click="$emit('toggle-add-task')" 
+            :text="showAddTask ? 'Close' : 'Add Task'" 
+            :color="showAddTask ? 'Red' : 'Green'" 
+        />
     </header>
 </template>
 
@@ -13,8 +17,8 @@ export default {
   props: {
     title: {
         type: String,
-        default: 'Task Manager'
     },
+    showAddTask: Boolean,
   },
   components: {
       Button
